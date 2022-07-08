2b75678a30588e1f333f98cd8cae58fe50843534
Daniel Schechtman <dschechtman@carthage.edu>
2022-07-07 23:00:09 -0500

    removing all tracked files on local machine to get ride of some files that are on github

--- tsc/old/version 1/src/Prototypes/Extended/ExtendedCreep.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExtendedCreep = void 0;
-class ExtendedCreep extends Creep {
-    Get(id) {
-        return Game.getObjectById(id);
-    }
-}
-exports.ExtendedCreep = ExtendedCreep;
--- tsc/old/version 1/src/types/Enums.ts
+++
@@ -1,7 +0,0 @@
-export enum TerrainTypes {
-    ROAD_TERRAIN,
-    PLAIN_TERRAIN,
-    SWAMP_TERRAIN,
-    OCCUPIED_TERRAIN,
-    WALL_TERRAIN,
-}
