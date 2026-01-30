a601eb0b3920114ef64426db8230da9190ac9b32
Daniel Schechtman <daschechtman@gmail.com>
2026-01-29 21:50:00 -0600

    updated upgrade behavior to also take energy from containers when available

--- src/Creeps/CreepBehaviors.ts/UpgraderBehavior.ts
+++ src/Creeps/CreepBehaviors.ts/UpgraderBehavior.ts
@@ -1,22 +1,29 @@
 import { CreepBehavior, JsonObj } from "Consts"
+import { FILE } from "dns"
 import { ScreepFile, ScreepMetaFile } from "FileSystem/File"
+import { RoomData } from "Rooms/RoomData"
+import { Timer } from "utils/Timer"
 import { SafeReadFromFileWithOverwrite } from "utils/UtilFuncs"
 
 export class UpgraderBehavior implements CreepBehavior {
     private data: JsonObj
     private state_key: string
+    private container_key: string
     private creep_id: string
     private creep: Creep | null
     private sources: Source[]
+    private containers: StructureContainer[]
     private controller: StructureController | undefined
 
     constructor() {
         this.state_key = "state key"
+        this.container_key = "from container?"
         this.data = {}
         this.sources = []
         this.controller = undefined
         this.creep_id = ""
         this.creep = null
+        this.containers = []
     }
 
     public Load(file: ScreepFile, id: string) {
@@ -29,6 +36,36 @@ export class UpgraderBehavior implements CreepBehavior {
         }
 
         this.data[this.state_key] = SafeReadFromFileWithOverwrite(file, this.state_key, false)
+        this.data[this.container_key] = SafeReadFromFileWithOverwrite(file, this.container_key, 'null')
+
+        const TIMER = new Timer(id)
+        TIMER.StartTimer(15)
+
+        if (this.data[this.container_key] === 'null' || TIMER.IsTimerDone()) {
+
+            const ALL_CONTAINERS = RoomData.GetRoomData().GetRoomStructures(STRUCTURE_CONTAINER)
+                .map(id => Game.getObjectById(id))
+                .filter(s => s !== null) as StructureContainer[]
+
+            const STORED_ENERGY = ALL_CONTAINERS.reduce((prev, cur) => prev + cur.store.getUsedCapacity(RESOURCE_ENERGY), 0)
+
+            if (STORED_ENERGY > 0 && STORED_ENERGY >= 2000 * ALL_CONTAINERS.length * .9) {
+                ALL_CONTAINERS.sort((a, b) => {
+                    return a.store.getUsedCapacity(RESOURCE_ENERGY) - b.store.getUsedCapacity(RESOURCE_ENERGY)
+                })
+
+                this.data[this.container_key] = ALL_CONTAINERS.at(-1)!.id
+
+            }
+            else if (STORED_ENERGY <= 2000 * ALL_CONTAINERS.length * .1) {
+                this.data[this.container_key] = 'N/A'
+            }
+
+            if (this.data[this.container_key] === 'null') {
+                this.data[this.container_key] = "N/A"
+            }
+        }
+
         return this.creep != null
     }
 
@@ -46,7 +83,11 @@ export class UpgraderBehavior implements CreepBehavior {
         }
 
         if (!this.data[this.state_key]) {
-            if (this.creep.harvest(this.sources[1]) === ERR_NOT_IN_RANGE) {
+            const CONTAINER = Game.getObjectById(this.data[this.container_key] as Id<StructureContainer>)
+            if (CONTAINER && this.creep.withdraw(CONTAINER, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                this.creep.moveTo(CONTAINER)
+            }
+            else if (this.creep.harvest(this.sources[1]) === ERR_NOT_IN_RANGE) {
                 this.creep.moveTo(this.sources[1], { maxRooms: 1 })
             }
         }
@@ -59,6 +100,7 @@ export class UpgraderBehavior implements CreepBehavior {
 
     public Cleanup(file: ScreepFile) {
         file.WriteToFile(this.state_key, this.data[this.state_key])
+        file.WriteToFile(this.container_key, this.data[this.container_key])
     }
 
 }
